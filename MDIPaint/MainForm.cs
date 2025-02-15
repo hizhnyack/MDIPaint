d9fb17a29606b72e2357796d3eaa4c7ff591c514
vboxuser <vboxuser@WIN10.ty>
2025-02-15 09:50:28 +0300

    Add project files.

---
+++ MDIPaint/MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDIPaint
+{
+    public partial class MainForm : Form
+    {
+        public static Color Color { get; set; }
+        public static int Width { get; set; }
+        public MainForm()
+        {
+            InitializeComponent();
+            Color = Color.Black;
+            Width = 3;
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frmAbout = new AboutForm();
+            frmAbout.ShowDialog();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void новыйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new DocumentForm();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+    }
+}
